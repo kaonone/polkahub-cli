4eeedc747e4ef2b76ff3c91721056f0dec30964e
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2019-12-12 13:49:54 +0200

    output: format user output

--- src/lib.rs
+++ src/lib.rs
@@ -42,10 +42,17 @@ impl Response {
     pub fn process(&self) {
         match self {
             Response::Success(s) => {
-                println!("success {:#?}", s);
+                let p = &s.payload;
+
+                println!("Success!");
+                println!("Repository available here:\n{:?}", p.repo_url);
+                println!("Endpoints:");
+                println!("https {:?}", p.http_url);
+                println!("https {:?}", p.ws_url);
+                println!("Success!");
             }
             Response::Fail(e) => {
-                println!("failed {:#?}", e);
+                println!("Could not create project. Reason: {:?}", e.reason);
             }
         }
     }
@@ -70,12 +77,10 @@ impl Project {
             self.project_name, self.account_id,
         );
         loader.set_msg("");
+
         let _ = loader.start();
-
         let result: Value = client.post(url).json(self).send().await?.json().await?;
-
         let _ = loader.stop();
-        println!("\n{:#?}", result);
 
         parse_response(result.to_string())
     }
@@ -84,29 +89,13 @@ impl Project {
 pub fn parse_response(r: String) -> Result<Response, reqwest::Error> {
     let response = match serde_json::from_str(&r) {
         Ok(r) => Response::Success(Success { ..r }),
-        Err(e) => Response::Fail(NotCreated {
-            status: "json parse error".to_owned(),
-            reason: e.to_string(),
-        }),
+        Err(_) => match serde_json::from_str(&r) {
+            Ok(r) => Response::Fail(NotCreated { ..r }),
+            Err(e) => Response::Fail(NotCreated {
+                status: "json parse error".to_owned(),
+                reason: e.to_string(),
+            }),
+        },
     };
     Ok(response)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    const P_ID: u64 = 5;
-    const P_NAME: &str = "NAME";
-    
-    #[test]
-    fn test_parse_works() {
-        let project = Project::from(P_ID, P_NAME.to_owned());
-        assert_eq!(
-            project,
-            Project {
-                account_id: P_ID,
-                project_name: String::from(P_NAME.to_owned()),
-            }
-        );
-    }
 }
