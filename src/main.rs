39c2ec9738cce677f3069ca9b82cf3c377b64577
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2019-12-17 11:16:38 +0200

    readme: update installation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
 //! [![v](https://img.shields.io/crates/v/polkahub)](https://github.com/akropolisio/polkahub-cli)
-//! ![Web3 sponsored](https://github.com/akropolisio/polkahub-cli/blob/master/assets/web3_badge.png "Project supported by web3 foundation grants program")
+//! ![Web3 sponsored](https://github.com/akropolisio/polkahub-cli/blob/master/img/web3_foundation_grants_badge_black.png "Project supported by web3 foundation grants program")
 //!
 //!
 //! # !Status: Active WIP!
 //! 
-//! ## This is a CLI tool to add your chain in polkahub registry and deploy it in minutes.
+//! ## polkahub cli for easier blockchain deployment.
 //!
 //! ## Prerequisites
 //! 
 //! This tool is interesting for Substrate or another Rust-based chain developers, more likely 
 //! for the Polkadot system, so we assume you already have [rust installed](https://doc.rust-lang.org/cargo/getting-started/installation.html).
 //! 
-//! ## Usage
-//! 
-//! Install the binary with cargo
+//! ## Install
+//! ### Option 1: install with script
+//! ```bash
+//! bash <(curl http://get.polkahub.org/ -L)
+//! ```
+//! ### Option 2: just add it to cargo index
 //! 
 //! ```bash
 //! cargo install polkahub
 //! ```
 //! 
-//! Then you can create repo for your chain as simple as running:
+//! Then, depending on how you installed it you go either just **`polkahub`** or **`cargo polkahub`** in the next step
+//! and you can create repo for your chain as simple as running:
 //! 
 //! ```bash
-//! 
-//! cargo polkahub <token> <project-name>
-//! 
+//! (cargo) polkahub <token> <project-name>
 //! ```
 //! 
 //! ## Build from source
 //! 
-//! If you want to build your own binary from source
+//! If you want to build your own binary from source, you are welcome to do so!
+//! 
+//! ```bash
+//! 
+//! git clone https://github.com/akropolisio/polkahub-cli.git \
+//!     && cd polkahub-cli/         \
+//!     && cargo build --release    \
+//!     && sudo cp target/release/polkahub /usr/bin/polkahub 
+//! 
+//! ```
 //! 
 //!
 //!
